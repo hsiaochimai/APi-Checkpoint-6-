1a18cd45aab305d5a02eea3fb25283acb207c35c
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-02-18 15:20:48 +0200

    more error handling

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-function getRepos(username){
-console.log(`https://api.github.com/users/${username}/repos`)
-fetch(`https://api.github.com/users/${username}/repos`)
-.then(response=> response.json())
-.then(obj=>{
-    console.log(obj)
-return displayResults(obj)
-})
+function getRepos(username) {
+    console.log(`https://api.github.com/users/${username}/repos`)
+    fetch(`https://api.github.com/users/${username}/repos`)
+        .then(response => response.json())
+        .then(obj => {
+            console.log(obj)
+            if (obj.message === 'Not Found') {
+                alert('Username not found, did you type it correctly?')
+            }
+            return displayResults(obj)
+        }).catch(e => {
+            alert(`Error: ${e.toString()}`)
+        })
 }
-function displayResults(responseArr){
-        const $results= $('#results')
-        $results.empty();
-        $results.removeClass('hidden')
-       if(responseArr.message==='Not Found') {
-           alert('Username not found, did you type it correctly?')
-       }
-        else{
-        for(let i=0; i<=responseArr.length-1; i++ ){
-      const repoName= `<p>Repo name: ${responseArr[i].name}</p>`
-      const repoUrl= `<p><a href=" ${responseArr[i].html_url}">Link</a></p>`
-    $results.append(repoName);
-    $results.append(repoUrl);
-}
-}
+function displayResults(responseArr) {
+    const $results = $('#results')
+    $results.empty();
+    $results.removeClass('hidden')
+
+    for (let i = 0; i <= responseArr.length - 1; i++) {
+        const repoName = `<p>Repo name: ${responseArr[i].name}</p>`
+        const repoUrl = `<p><a href=" ${responseArr[i].html_url}">Link</a></p>`
+        $results.append(repoName);
+        $results.append(repoUrl);
     }
 
+}
 
 
 
@@ -32,14 +33,15 @@ function displayResults(responseArr){
 
 
 
-function setupSearch(){
-$('form').submit(event=>{
-    event.preventDefault();
-    console.log('submit button was clicked')
-    const userInputUsername= document.getElementById('js-username').value 
-    console.log(`User Input is ${userInputUsername}`)
-    $('#js-username').val('')
-    getRepos(userInputUsername);
-})
+
+function setupSearch() {
+    $('form').submit(event => {
+        event.preventDefault();
+        console.log('submit button was clicked')
+        const userInputUsername = document.getElementById('js-username').value
+        console.log(`User Input is ${userInputUsername}`)
+        $('#js-username').val('')
+        getRepos(userInputUsername);
+    })
 }
 setupSearch();
